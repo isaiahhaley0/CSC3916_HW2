81462cbb1c131018d6ffcd3f16a8ec240bac0c22
isaiahhaley0 <isaiah.haley@ucdenver.edu>
2021-02-27 17:32:46 -0700

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var jwt = require('jsonwebtoken');
 var cors = require('cors');
 
 var app = express();
-app.use(cors());
+//app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
