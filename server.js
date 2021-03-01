4fd827f9c0def937d310009dc31a4fdfa3810c86
isaiahhaley0 <isaiah.haley@ucdenver.edu>
2021-02-28 19:42:40 -0700

    Update server.js

--- server.js
+++ server.js
@@ -135,7 +135,7 @@ router.route('/movies')
     })
     .put(authController.isAuthenticated, function(req, res) {
         console.log(req.body);
-        res = res.status(200).send({success:true,msg:'saved'});
+        res = res.status(200).send({success:true,msg:'updated'});
         if (req.get('Content-Type')) {
             res = res.type(req.get('Content-Type'));
         }
